9fb5a1c5cd311ca54746b82630f7611e6199a974
Daniel Gentil <danielgentil2003@gmail.com>
2020-05-19 09:08:18 -0300

    Score de volume de compras

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-double compra, ticketpadrao , pagamentos , atraso ;
+double compra, ticketpadrao , pagamentos , atraso, pts ;
 
 cout << endl << "SISTEMA DE PERFIL DO CLIENTE" << endl;
 cout << "--------------------------------" << endl;
@@ -19,6 +19,19 @@ cin >> atraso;
 cout << endl << "A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)?" ;
 cin >> pagamentos;
 
+if (ticketpadrao == 0) {
+        pts = 0;
+    }
+    else if (compra <= 2 && ticketpadrao <= 3000) {
+        pts = 20;
+    }
+    else if (compra > 2 && ticketpadrao <= 3000 ) {
+        pts = 40;
+    }
+    else {
+        pts = 60;
+    }
 
+    cout << endl << "Score de volume de compras = " << ticketpadrao << " pontos" << endl;
  return 0;
 }
