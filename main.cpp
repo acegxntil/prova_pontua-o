4212074cf84f68777d1f745e9369861f00775cf4
Daniel Gentil <danielgentil2003@gmail.com>
2020-05-19 08:40:20 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -3,9 +3,18 @@ using namespace std;
 
 int main()
 {
+double compra, ticketpadrao , pagamentos , atraso ;
+
 cout << endl << "SISTEMA DE PERFIL DO CLIENTE" << endl;
 cout << "--------------------------------" << endl;
 cout << "INFORME OS DADOS DO ÚLTIMO ANO" << endl;
+
+cout << "Quantas compras o cliente fez no último ano?" ;
+cin >> compra ;
+cout << endl << "Qual o ticket médio? ";
+cin >> ticketpadrao;
+
+
 
  return 0;
 }
