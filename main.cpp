08c904bc8ab97d214f20693f060e7e97ffd12bc6
Daniel Gentil <danielgentil2003@gmail.com>
2020-05-19 08:21:32 -0300

    Cabecalho

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main()
 {
-
+cout << endl << "SISTEMA DE PERFIL DO CLIENTE" << endl;
+cout << "--------------------------------" << endl;
+cout << "INFORME OS DADOS DO ÚLTIMO ANO" << endl;
 
  return 0;
 }
